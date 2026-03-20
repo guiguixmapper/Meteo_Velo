cdeb1177689dc8f442bc57b5654f804ccec76c49
guiguixmapper <guillaume.ferrand34@gmail.com>
2026-03-20 09:09:33 +0100

    Update metrics_banner.py

--- velo_meteo/ui/components/metrics_banner.py
+++ velo_meteo/ui/components/metrics_banner.py
@@ -26,38 +26,31 @@ def render_metrics_banner(score: dict, dist_tot: float, d_plus: float, d_moins: 
       <div class="metric-grid">
         <div class="metric-cell">
           <div class="mv">{round(dist_tot/1000,1)}</div>
-          <div class="mu">km</div>
-          <div class="ml">Distance</div>
+          <div class="mu">km · Distance</div>
         </div>
         <div class="metric-cell">
           <div class="mv">{int(d_plus)}</div>
-          <div class="mu">m</div>
-          <div class="ml">D+</div>
+          <div class="mu">m · D+</div>
         </div>
         <div class="metric-cell">
           <div class="mv">{int(d_moins)}</div>
-          <div class="mu">m</div>
-          <div class="ml">D−</div>
+          <div class="mu">m · D−</div>
         </div>
         <div class="metric-cell">
           <div class="mv">{dh}h{dm:02d}</div>
-          <div class="mu">&nbsp;</div>
-          <div class="ml">Durée</div>
+          <div class="mu">Durée</div>
         </div>
         <div class="metric-cell">
           <div class="mv orange">{vit_moy_reelle}</div>
-          <div class="mu">km/h</div>
-          <div class="ml">Moy. réelle</div>
+          <div class="mu">km/h · Moy.</div>
         </div>
         <div class="metric-cell">
           <div class="mv">{heure_arr.strftime('%H:%M')}</div>
-          <div class="mu">&nbsp;</div>
-          <div class="ml">Arrivée</div>
+          <div class="mu">Arrivée</div>
         </div>
         <div class="metric-cell">
           <div class="mv">{calories}</div>
           <div class="mu">kcal</div>
-          <div class="ml">Calories</div>
         </div>
       </div>
     </div>""", unsafe_allow_html=True)
